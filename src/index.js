932a224862a6d3b5fd9b58c292b34d177c584719
Veronica Woodward (lannstoirme on Discord) <46139638+lannstoirme@users.noreply.github.com>
2021-05-29 15:15:38 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import {configureStore,combineReducers} from '@reduxjs/toolkit'
 
 
+// Reducers
+import authReducer from './Controllers/Redux/authSlice';
+import bugReducer from './Controllers/Redux/bugSlice';
+import userReducer from './Controllers/Redux/userSlice';
+
+// Redux Configure 
+const reducer = combineReducers({
+  auth:authReducer,
+  bug:bugReducer,
+  user:userReducer,
+})
+
+const store = configureStore({
+  reducer
+})
 ReactDOM.render(
-  
-    <App />,
+    <Provider store={store}>
+    <App />
+    </Provider>,
   
   document.getElementById('root')
 );
-
 
