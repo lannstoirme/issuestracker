07eafa333ef05bca3aed031c1091e5364e0d6a0a
Veronica Woodward (lannstoirme on Discord) <46139638+lannstoirme@users.noreply.github.com>
2021-05-31 12:04:18 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import './App.css';
+import {useSelector} from 'react-redux';
+import Login from './Views/Login/login';
 
 function App() {
+  const {auth} = useSelector(state => state)
   return (
    <>
+   {!auth.loggedIn ? <Login /> :
+   <h1>Welcome</h1>
+   }
    </>
   );
 }
 
 export default App;
+
