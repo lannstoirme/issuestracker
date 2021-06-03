cac33bfdc924ea9d617b701dbf980c5ec30a24e0
Veronica Woodward (lannstoirme on Discord) <46139638+lannstoirme@users.noreply.github.com>
2021-06-03 15:09:14 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import Login from './Views/Login/login';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import ViewBugPage from './Views/Pages/viewBugs';
+import SideBar from './Views/SideBar/sideBar';
 
 function App() {
   const {auth} = useSelector(state => state)
   return (
-   <>
+   <Router>
    {!auth.loggedIn ? <Login /> :
-   <h1>Welcome</h1>
-   }
+   <>
+      <h1>Welcome</h1>
+      <ViewBugPage />
    </>
+   }
+   </Router>
   );
 }
 
 export default App;
-
--- src/Views/SideBar/sideBar.jsx
+++ src/Views/SideBar/sideBar.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
 
-export default ()=>{
+import {signOut} from '../../Controllers/Redux/authSlice';
+import './sideBar.css';
+
+export default () => {
+    const dispatch = useDispatch();
+
+    const { auth } = useSelector(state => state)
+
+    function SignOut() {
+        dispatch(signOut());
+    }
+
     return(
         <div className="sidebar">
+            <Link className="nav-link" to="/"><h1 className="brand">Bug-Tracker</h1></Link>
+                <ul>
+                    <li><link to='/' className='nav-link'>Dashboard</link></li>
+                    <li><link to='/viewbugs' className='nav-link'>View Bugs</link></li>
+                    {auth.admin && <li><link to='/create' className='nav-link'>Create Bug</link></li>}
+                </ul>
+                <button className='nav-link logout'>Logout</button>
         </div>
     )
 }
